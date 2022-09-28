695caf6e488b6cb2bd80d375831c17fbfeec3c9a
Tatiana Rudikova <tatjana_tv@list.ru>
2022-09-28 15:39:31 +0300

    added project

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+        int mile = 20;
+        int ticket = 100;
+
+        int bonusMiles = (ticket / mile);
+
+        System.out.println("Начислено бонусных милей " + bonusMiles);
+    }
+}
